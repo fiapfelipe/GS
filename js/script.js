a2a1398dc4bafb3dbb9797a8fe03e6e9893e03fe
fiapfelipe <rm559175@fiap.com.br>
2024-11-18 20:21:46 -0300

    [200~

---
+++ js/script.js
@@ -0,0 +1,37 @@
+//MENU
+const iconeLista = document.querySelector('.bi-list')
+const listaLinks = document.querySelector('#lista_nav')
+
+function ocultaLista(){
+    listaLinks.classList.toggle('menu')
+}
+
+iconeLista.addEventListener('click', ocultaLista)
+
+
+//BARRAS DO GRAFICO
+const tabs = document.querySelectorAll('.tab-btn');
+
+tabs.forEach(tab => tab.addEventListener('click', () => tabClicked(tab)));
+
+const tabClicked = (tab) => {
+    tabs.forEach(tab => tab.classList.remove('active'));
+    tab.classList.add('active');
+
+    const contents = document.querySelectorAll('.content');
+    contents.forEach(content => content.classList.remove('show'));
+
+    const contentId = tab.getAttribute('content-id');
+    const content = document.getElementById(contentId);
+
+    content.classList.add('show');
+}
+
+const currentActiveTab = document.querySelector('.tab-btn.active');
+tabClicked(currentActiveTab);
+
+
+//CONTATO
+document.getElementById("btn_submit").addEventListener("click", function() {
+    alert("Enviado com sucesso!");
+});
